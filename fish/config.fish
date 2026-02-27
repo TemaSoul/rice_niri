ae2fc8dc4c834a89a843d95ff2da6dbd70cedf13
TemaSoul <stepanov93555@gmail.com>
2026-02-27 07:14:48 +0300

    preventive update

--- fish/config.fish
+++ fish/config.fish
@@ -6,15 +6,18 @@ alias reflector-update='doas reflector --country "RU,GE,BY,AZ" --latest 10 --sor
 alias zlj="zellij"
 alias steam="wl-copy --clear; env DISPLAY=:0 /usr/bin/steam"
 # Видео: только контент, без вшитых картинок
-alias yt-dlp_va='yt-dlp -4 -f "bestvideo+bestaudio/best" --no-mtime --merge-output-format mp4'
+alias yt-dlp_va='yt-dlp --cookies-from-browser=firefox -4 -f "bestvideo+bestaudio/best" --no-mtime --merge-output-format mp4'
 # Аудио: чистый звук в m4a (лучшее качество без лишней перекодировки)
-alias yt-dlp_a='yt-dlp -4 -f "bestaudio/best" --no-mtime --extract-audio --audio-format m4a'
+alias yt-dlp_a='yt-dlp --cookies-from-browser=firefox -4 -f "bestaudio/best" --no-mtime --extract-audio --audio-format m4a'
 # Онлайн просмотри для YouTube
 alias mpv_yt='mpv --ytdl-raw-options=force-ipv4="" '
 # root iftop
 alias iftop='doas iftop'
 # poweroff
 alias poweroff='systemctl poweroff'
+# send2phone
+alias send2phone 'kdeconnect-cli --device 976c9f2853ad426a8300529bc2c96b74 --share'
+
 
 # переменные
 set -gx DISPLAY :0
